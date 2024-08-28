a232a47d819c9213254c68ac2c1040a6f25e1eb0
Brendo Faisca <bmlfaisca@gmail.com>
2024-08-28 14:21:29 -0300

    Add project files.

---
+++ src/CashFlow.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Communication.Responses;
+public class ResponseErrorJson
+{
+    public string ErrorMessage { get; set; } = string.Empty;
+
+    public ResponseErrorJson(string erroMessage)
+    {
+         ErrorMessage = erroMessage;
+    }
+}
---
+++ src/ClashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,42 @@
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace ClashFlow.Application.UseCases.Expenses.Register;
+public class RegisterExpenseUseCase
+{
+    public ResponseRegistredExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        Validate(request);
+        return new ResponseRegistredExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var titleEmpty = string.IsNullOrWhiteSpace(request.TItle);
+
+        if (titleEmpty)
+        {
+            throw new ArgumentException("The title is required.");
+        }
+
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException("The Amount must be greater than zero.");
+        }
+
+        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+
+        if (result > 0)
+        {
+            throw new ArgumentException("Expenses cannot be for the future.");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType),request.PaymentType);
+
+        if (!paymentTypeIsValid)
+        {
+            throw new ArgumentException("Payment type is not valid.");
+        }
+    }
+}
